ed081dca78225e55378f6e9c2bd354f69d89a679
이창섭 <ventulus95@gmail.com>
2022-05-14 16:24:26 +0900

    feat: Wrapper Response 객체 구현
    - HttpResponse.kt 구
    - ResponseEntity를 통한 Response 추가

--- src/main/kotlin/com/ddd/ddoit/controller/AuthController.kt
+++ src/main/kotlin/com/ddd/ddoit/controller/AuthController.kt
@@ -3,7 +3,11 @@ package com.ddd.ddoit.controller
 import com.ddd.ddoit.domain.User
 import com.ddd.ddoit.domain.UserService
 import com.ddd.ddoit.dto.AuthRequest
+import com.ddd.ddoit.dto.HttpResponse
 import com.ddd.ddoit.jwt.JwtTokenProvider
+import org.apache.coyote.Response
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.context.SecurityContextHolder
@@ -16,13 +20,17 @@ import org.springframework.web.bind.annotation.RestController
 class AuthController(val userService: UserService, val jwtTokenProvider: JwtTokenProvider) {
 
     @PostMapping("/signup")
-    fun signUp(@RequestBody req: AuthRequest): Long? {
-        return userService.signupUser(req)
+    fun signUp(@RequestBody req: AuthRequest): ResponseEntity<HttpResponse<Long?>> {
+        return ResponseEntity(HttpResponse(
+            200,"회원 가입 완료", userService.signupUser(req)),
+            HttpStatus.OK)
     }
 
     @PostMapping("/login")
-    fun login(@RequestBody req: AuthRequest): String{
-        return jwtTokenProvider.createToken(userService.login(req).email, listOf())
+    fun login(@RequestBody req: AuthRequest): ResponseEntity<HttpResponse<String>>{
+        return ResponseEntity(HttpResponse(
+            200, "로그인 완료", jwtTokenProvider.createToken(userService.login(req).email, listOf())
+        ), HttpStatus.OK)
     }
 
     @GetMapping("/test")
