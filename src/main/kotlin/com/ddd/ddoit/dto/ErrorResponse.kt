a82d659d4874f5d27cebf0b5a658b01ba79f6a4c
이창섭 <ventulus95@gmail.com>
2022-05-28 16:02:53 +0900

    feat: 에러코드명 변경, 에러 형식 변경
    - 에러 코드 형식 변경
    - 익셉션 처리 변경

--- src/main/kotlin/com/ddd/ddoit/dto/ErrorResponse.kt
+++ src/main/kotlin/com/ddd/ddoit/dto/ErrorResponse.kt
@@ -1,7 +1,7 @@
 package com.ddd.ddoit.dto
 
 class ErrorResponse<T> (val code: Int,
-                        val ex: String,
+                        val name: String,
                         val msg: T
 ){
     val trace: String? = null
--- src/main/kotlin/com/ddd/ddoit/exception/BaseErrorCodeException.kt
+++ src/main/kotlin/com/ddd/ddoit/exception/BaseErrorCodeException.kt
@@ -5,8 +5,8 @@ import org.springframework.http.HttpStatus
 enum class BaseErrorCodeException (val status: HttpStatus, val message: String, val code: Int) {
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다.", 700),
 
-    INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "잘못된 비밀번호입니다. 다시 입력해주세요.", 901),
-    DUPLICATE_EMAIL(HttpStatus.BAD_REQUEST, "중복된 이메일입니다. 다시 입력해주세요." ,900),
+    INVALID_USER(HttpStatus.BAD_REQUEST, "유저 혹은 비밀번호가 일치하지 않습니다. 다시 입력해주세요.", 900),
+    DUPLICATE_NAME(HttpStatus.BAD_REQUEST, "중복된 이름입니다. 다시 입력해주세요." ,901),
 
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다.", 800),
     FAILED_TO_SAVE_USER(HttpStatus.NOT_FOUND, "사용자를 등록에 실패했습니다.", 801),
