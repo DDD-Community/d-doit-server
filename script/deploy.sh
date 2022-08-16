0d5314f11778ae8b8d59e113693bec62d7696ec8
이창섭 <ventulus95@gmail.com>
2022-08-16 21:28:26 +0900

    chore: 베포 스크립트 변경
    - 배포 방식 최적화 추가...

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,4 +1,17 @@
-CURRENT_PID=$(lsof -i :8080 | grep "LISTEN" | awk '{print $2}')
+if [ "$DEPLOYMENT_GROUP_NAME" == "d-doit-deploy-prod" ]
+then
+  DEPLOYMENT_NAME = "운영"
+  DEPLOYMENT_PORT = 8080
+  SERVICE_NAME = "d-doit-prod"
+fi
+if [ "$DEPLOYMENT_GROUP_NAME" == "d-doit-deploy-dev" ]
+then
+  DEPLOYMENT_NAME = "개발"
+  DEPLOYMENT_PORT = 8080
+  SERVICE_NAME = "d-doit"
+fi
+
+CURRENT_PID=$(lsof -i :$DEPLOYMENT_PORT | grep "LISTEN" | awk '{print $2}')
 
 if [ -z $CURRENT_PID ]
 then
@@ -13,14 +26,5 @@ fi
 echo "##############"
 echo "> 배포 시작"
 echo "##############"
-if [ "$DEPLOYMENT_GROUP_NAME" == "d-doit-deploy-prod" ]
-then
-  echo "운영 배포 시작"
-  systemctl start d-doit-prod.service
-fi
 
-if [ "$DEPLOYMENT_GROUP_NAME" == "d-doit-deploy-dev" ]
-then
-  echo "개발 배포 시작."
-  systemctl start d-doit.service
-fi
+systemctl start $SERVICE_NAME.service
